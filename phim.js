76291ce23d7488300099c69c0306144f630ef2e4
nhanquatot <nhanquatot@gmail.com>
2026-09-21 10:33:13 +0700

    Add MOVIES variable with movie details

---
+++ phim.js
@@ -0,0 +1,7 @@
+///ô đầu tiên là đường link
+///ô hai hiển thị tên ra trang chủ
+///ô ba là ảnh bìa
+var MOVIES = 
+'lozlfve-the-fairy-of-qiong|the-fairy-of-qiong|https://rule34video.com/contents/videos_screenshots/3949000/3949013/336x189/3.jpg|Tập 16\n' +
+'phim-b|Tên phim B|https://i.imgur.com/FFswBs1.jpeg|Full\n' +
+'phim-c|Tên phim C|https://i.imgur.com/FFswBs1.jpeg|HD';
